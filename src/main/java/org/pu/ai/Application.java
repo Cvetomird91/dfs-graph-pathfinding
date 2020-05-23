ee9532ef351c60edf16525d7b0d41212585650bb
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2020-05-23 16:04:55 +0300

    move method for reading links and nodes to Graph object

--- src/main/java/org/pu/ai/Application.java
+++ src/main/java/org/pu/ai/Application.java
@@ -1,9 +1,5 @@
 package org.pu.ai;
 
-import org.pu.ai.model.Node;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Application {
@@ -11,34 +7,7 @@ public class Application {
     static Graph graph = new Graph();
 
     public static void init(Graph g) throws IOException {
-        readData(g);
-    }
-
-    private static void readData(Graph graph) throws IOException {
-        BufferedReader linkReader = new BufferedReader(new FileReader("src/resources/links.grf"));
-        BufferedReader nodeReader = new BufferedReader(new FileReader("src/resources/nodes.grf"));
-
-        //retrieve and assign node data
-        String nodeRow;
-        while ((nodeRow = nodeReader.readLine()) != null) {
-            // remove trailing semi - colon at the end of the line
-            String line = nodeRow.substring(0, nodeRow.length() - 1);
-            String[] data = line.split(", ");
-            Node node = new Node(data[0], Integer.parseInt(data[1]));
-            graph.addNode(node);
-        }
-
-        //retrieve and assign link data
-        String linkRow;
-        while ((linkRow = linkReader.readLine()) != null) {
-            // remove trailing semi - colon at the end of the line
-            String line = linkRow.substring(0, linkRow.length() - 1);
-            String[] data = line.split(", ");
-            graph.addLink(data[0], data[1], Boolean.parseBoolean(data[2]), Integer.parseInt(data[3]));
-        }
-
-        linkReader.close();
-        nodeReader.close();
+        g.readData("src/resources/links.grf", "src/resources/nodes.grf");
     }
 
     public static void main(String[] args) throws IOException{
--- src/main/java/org/pu/ai/Graph.java
+++ src/main/java/org/pu/ai/Graph.java
@@ -3,6 +3,9 @@ package org.pu.ai;
 import org.pu.ai.model.Link;
 import org.pu.ai.model.Node;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -49,6 +52,33 @@ public class Graph {
 
     public List<Link> getLink(String name) {
         return myGraph.get(name).getLinks();
+    }
+
+    public void readData(String linksFilePath, String nodesFilePath) throws IOException {
+        BufferedReader linkReader = new BufferedReader(new FileReader(linksFilePath));
+        BufferedReader nodeReader = new BufferedReader(new FileReader(nodesFilePath));
+
+        //retrieve and assign node data
+        String nodeRow;
+        while ((nodeRow = nodeReader.readLine()) != null) {
+            // remove trailing semi - colon at the end of the line
+            String line = nodeRow.substring(0, nodeRow.length() - 1);
+            String[] data = line.split(", ");
+            Node node = new Node(data[0], Integer.parseInt(data[1]));
+            addNode(node);
+        }
+
+        //retrieve and assign link data
+        String linkRow;
+        while ((linkRow = linkReader.readLine()) != null) {
+            // remove trailing semi - colon at the end of the line
+            String line = linkRow.substring(0, linkRow.length() - 1);
+            String[] data = line.split(", ");
+            addLink(data[0], data[1], Boolean.parseBoolean(data[2]), Integer.parseInt(data[3]));
+        }
+
+        linkReader.close();
+        nodeReader.close();
     }
 
 }//end class Graph
